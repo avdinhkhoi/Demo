659619df715a11cbc16444d85cbedd3c0b117df9
avdinhkhoi <avdinhkhoi@gmail.com>
2021-05-10 00:04:03 +0700

    edit path

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".navmenu");
-const navBar = document.querySelector(".navbar");
-
-menuIcon.addEventListener("click", () => {
-    navBar.classList.toggle("change");
-});
